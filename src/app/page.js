46e340904b5eaf4d300a2e2253805bc2e3ed3d4a
Ermias Tesfaye <ermiastesfaye28@gmail.com>
2024-08-18 15:14:25 +0300

    home page updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
-
-import { EnvelopeOpenIcon } from '@radix-ui/react-icons';
-
+import { EnvelopeOpenIcon, Link1Icon, LinkedInLogoIcon } from '@radix-ui/react-icons';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+      <Link href={'/recipes'}><Link1Icon/>Go To the APP</Link>
     </main>
   );
 }
